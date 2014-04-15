20126015d16ad197c92ff77395f73295156d1553
Brian Kelly <polymonic@gmail.com>
2014-04-15 11:10:06 -0400

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularXhtmlPoc::Application.config.secret_key_base = '52134f4eac82c10a8369408b58ebe4a23ffb1d652ff3a6083052f3f6fc95fc53f280705fa2e03ad79ea30d395603c376a809921d17224c84d5c50a2e32c99ae6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AngularXhtmlPoc::Application.config.session_store :cookie_store, key: '_angular-xhtml-poc_session'
